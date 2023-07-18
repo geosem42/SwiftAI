5e3cf103279b8d34eda9211274ba09f405055ce6
Mike Karthauser <mike.karthauser@bookinglive.co.uk>
2023-07-18 22:31:49 +0100

    adds some test coverage for ChatController methods

--- tests/Feature/Controllers/ChatControllerTest.php
+++ tests/Feature/Controllers/ChatControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Controllers;
 
 use App\Models\Conversation;
+use App\Models\Personality;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -10,6 +11,43 @@ use Tests\TestCase;
 class ChatControllerTest extends TestCase
 {
     use RefreshDatabase;
+
+    /** @test */
+    public function test_can_create_new_conversation_fails_validation()
+    {
+        $user = User::factory()->create();
+
+        $this
+            ->actingAs($user)
+            ->postJson('/new-conversation', [])->assertUnprocessable()
+            ->assertJson([
+                "title" => [
+                    "The title field is required."
+                ],
+                "personality_id" => [
+                    "The personality id field is required."
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function test_can_create_new_conversation()
+    {
+        $user = User::factory()->create();
+
+        $payload = [
+            'title' => 'Title',
+            'personality_id' => Personality::factory()->create()->id,
+        ];
+
+        $this
+            ->actingAs($user)
+            ->postJson('/new-conversation', $payload)
+            ->assertSuccessful()
+            ->assertJsonFragment($payload + [
+                    'user_id' => $user->id
+                ]);
+    }
 
     /** @test */
     public function test_can_get_conversations()
@@ -26,5 +64,19 @@ class ChatControllerTest extends TestCase
             ->assertSuccessful()
             ->assertJsonFragment($conversations->first()->toArray());
 
+    }
+
+    /** @test */
+    public function test_can_get_personalities()
+    {
+        $personalities = Personality::factory()->times(4)->create();
+
+        $user = User::factory()->create();
+
+        $this
+            ->actingAs($user)
+            ->getJson('/personalities')->assertSuccessful()
+            ->dump()
+            ->assertJsonFragment($personalities->first()->toArray());
     }
 }
